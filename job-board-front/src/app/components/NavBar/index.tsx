3b7cace01ad7cc8d6ac1d831a32db3a88ace080c
Hugo Delattre <delattre.hugo.24@gmail.com>
2023-10-18 17:35:47 +0200

    style: profile page

--- job-board-front/src/app/components/NavBar/index.tsx
+++ job-board-front/src/app/components/NavBar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { useAuthStore } from "@/app/store/auth-store";
 import { useRouter } from "next/navigation";
@@ -35,30 +35,11 @@ const NavBar = () => {
           JobBoard
         </Link>
         <div className={styles.nav_middle}>
-          {/* <Link href="/companies">Companies</Link> */}
         </div>
         {!isLoggedIn && (
           <div className={styles.nav_right}>
             <ButtonLight text="Login" url="/login" />
-
-            {/* <Link
-              href="/login"
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group 
-            bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800
-            "
-            >
-              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Login
-              </span>
-            </Link> */}
-            {/* <Button text="Register" url="/register" /> */}
             <Button text="Register" url="/register" />
-            {/* <Link
-              href="/register"
-              className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl transition-all focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            >
-              Register
-            </Link> */}
           </div>
         )}
 
@@ -75,16 +56,8 @@ const NavBar = () => {
               </span>
             </button>
             <Button text="My Profile" url="/profile" />
-            {/* <Link
-              href="/profile"
-              className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl transition-all focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            >
-              My Profile
-            </Link> */}
           </div>
         )}
-
-        {/* {isLoggedIn && isAdmin && ()} */}
       </nav>
     </div>
   );
--- job-board-front/src/app/profile/page.tsx
+++ job-board-front/src/app/profile/page.tsx
@@ -3,6 +3,8 @@
 import React, { useEffect, useState } from "react";
 import { getProfileId } from "@/app/utils/auth";
 import { useForm } from "react-hook-form";
+import Image from "next/image";
+import { LiaEditSolid, LiaSaveSolid } from "react-icons/lia";
 
 interface UserData {
   data: {
@@ -24,8 +26,6 @@ const ProfilePage = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [editableUserData, setEditableUserData] = useState<UserData | {}>({});
   const { register, handleSubmit } = useForm();
-  const [errorMessage, setErrorMessage] = useState("");
-  
 
   useEffect(() => {
     const userId = getProfileId();
@@ -43,101 +43,141 @@ const ProfilePage = () => {
   }, [isEditing]);
 
   return (
-    <div className="collapse bg-base-200">
-      {!("data" in userData) && (
-        <div className="flex justify-center my-60">
-          <span className="loading loading-spinner loading-lg"></span>
-        </div>
-      )}
-      {"data" in userData && isEditing && (
-        <form
-          className="p-4 flex gap-4 flex-col rounded"
-          onSubmit={handleSubmit(async (data) => {
-            try {
-              console.log("data", data);
-              console.log("userData", userData.data);
-
-              const res = await fetch(
-                `http://localhost:8000/users/${userData.data.id}`,
-                {
-                  method: "PUT",
-                  body: JSON.stringify(data),
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                }
-              );
-              const json = await res.json;
-              console.log("json", json);
-              if (res.ok) {
-                setIsEditing(false);
-                console.log("user updated successfully");
-              } else {
-                console.log("error updating user");
-              }
-            } catch (error) {
-              console.log("error updating user:", error);
-            }
-          })}
-        >
-          <p>User ID: {userData.data.id}</p>
-          <input
-            type="hidden"
-            value={userData.data.id}
-            {...register("id", { required: true })}
-          />
-          <label>
-            Email:
-            <input
-              type="email"
-              defaultValue={userData.data.email}
-              {...register("email", { required: false })}
-            />
-          </label>
-          <label>
-            First name:
-            <input
-              type="text"
-              defaultValue={userData.data.firstName}
-              {...register("firstName", { required: false })}
-            />
-          </label>
-          <label>
-            Last name:
-            <input
-              type="text"
-              defaultValue={userData.data.lastName}
-              {...register("lastName", { required: false })}
-            />
-          </label>
-          <button
-            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 leading-4 rounded-lg group
-              bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800
-              "
-            type="submit"
-          >
-            Save
-          </button>
-        </form>
-      )}
-      {"data" in userData && !isEditing && (
-        <div>
-          <div className="p-4 flex gap-4 flex-col">
-            <p>User ID: {userData.data.id}</p>
-            <p>Email: {userData.data.email}</p>
-            <p>First name: {userData.data.firstName}</p>
-            <p>Last name: {userData.data.lastName}</p>
-            <button
-              onClick={() => setIsEditing(true)}
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 leading-4 rounded-lg group
-              bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800
-              "
-            >
-              Edit
-            </button>
+    <div className="px-16 md:px-32 lg:px-64">
+      {/* <h3 className="text-xl"> */}
+      {/* Welcome to your profile page. */}
+      {/* {userData && {userData}} */}
+      {/* </h3> */}
+      {/* <p className="mb-4">Feel free to edit your personal informations.</p> */}
+      <div className="border border-white border-solid border-1 text-lg border rounded-lg ">
+        {!("data" in userData) && (
+          <div className="flex justify-center my-60">
+            <span className="loading loading-spinner loading-lg"></span>
           </div>
-        </div>
-      )}
+        )}
+
+        {"data" in userData && isEditing && (
+          <form
+            className="flex flex-col rounded "
+            onSubmit={handleSubmit(async (data) => {
+              try {
+                console.log("data", data);
+                console.log("userData", userData.data);
+                const res = await fetch(
+                  `http://localhost:8000/users/${userData.data.id}`,
+                  {
+                    method: "PUT",
+                    body: JSON.stringify(data),
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                  }
+                );
+                const json = await res.json;
+                console.log("json", json);
+                if (res.ok) {
+                  setIsEditing(false);
+                  console.log("user updated successfully");
+                } else {
+                  console.log("error updating user");
+                }
+              } catch (error) {
+                console.log("error updating user:", error);
+              }
+            })}
+          >
+            <div>
+              <div>
+                <div className="p-4 px-22 flex py-2 gap-4 flex-col justify-center md:flex-row items-center bg-slate-800 rounded-t-lg">
+                  <Image
+                    src="https://cdn.discordapp.com/attachments/1081136403137712150/1082991411340791958/TravelSquadDefaultUserImage.webp?ex=65423959&is=652fc459&hm=24958223005562882e48ab9037a3230c40bfd930b5085c0ebdc479d5100f1181&"
+                    alt="profile picture"
+                    width={200}
+                    height={200}
+                    className="filter brightness-0 invert"
+                  />
+                  <div>
+                    <div>
+                      <label>
+                        First name:
+                        <input
+                          type="text"
+                          defaultValue={userData.data.firstName}
+                          {...register("firstName", { required: false })}
+                        />
+                      </label>
+                    </div>
+                    <div>
+                      <label>
+                        Last name:
+                        <input
+                          type="text"
+                          defaultValue={userData.data.lastName}
+                          {...register("lastName", { required: false })}
+                        />
+                      </label>
+                    </div>
+                    <label>
+                      Email:
+                      <input
+                        type="email"
+                        defaultValue={userData.data.email}
+                        {...register("email", { required: false })}
+                      />
+                    </label>
+                    {/* <p>User ID: {userData.data.id}</p>
+                    <input
+                      type="hidden"
+                      value={userData.data.id}
+                      {...register("id", { required: true })}
+                    /> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="border border-white border-solid rounded-b-lg bg-slate-800 flex justify-center py-2 bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                <button onClick={() => setIsEditing(true)} className="">
+                  <LiaSaveSolid className="w-6 h-6" />
+                </button>
+              </div>
+            </div>
+          </form>
+        )}
+        {"data" in userData && !isEditing && (
+          <div>
+            <div>
+              <div>
+                <div className="p-4 px-22 flex gap-4 flex-col justify-center md:flex-row items-center bg-slate-800 rounded-t-lg">
+                  <Image
+                    src="https://cdn.discordapp.com/attachments/1081136403137712150/1082991411340791958/TravelSquadDefaultUserImage.webp?ex=65423959&is=652fc459&hm=24958223005562882e48ab9037a3230c40bfd930b5085c0ebdc479d5100f1181&"
+                    alt="profile picture"
+                    width={200}
+                    height={200}
+                    className="filter brightness-0 invert"
+                  />
+                  <div>
+                    <p className="text-4xl mb-2 font-semibold">
+                      {userData.data.firstName} {userData.data.lastName}
+                    </p>
+
+                    <p>Email: {userData.data.email}</p>
+                    <p>User ID: {userData.data.id}</p>
+                    <p>Role: {userData.data.role}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="border border-white border-solid rounded-b-lg bg-slate-800 flex justify-center py-2 bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                <button onClick={() => setIsEditing(true)} className="">
+                  <LiaEditSolid className="w-6 h-6" />
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
